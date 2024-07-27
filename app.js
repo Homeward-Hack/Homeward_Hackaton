2b22bbae83838a8462f1a29a50cc5b5236afefee
Ajaiyeoba <ajaiyeobajbola@gmail.com>
2024-07-27 13:51:33 +0100

    added the properrrty lists

--- app.js
+++ app.js
@@ -5,3 +5,62 @@ toggles.forEach(toggle => {
 		toggle.parentNode.classList.toggle('active');
 	});
 });
+
+
+// Property API 
+
+const url = "hi"
+const key = "12"
+const api = url + key;
+//fetch(url)
+console.log(api);
+
+// fetch(api)
+// .then((response => response.json))
+// .then(data  => 
+// 	console.log(data)
+// 	{}
+// 	)
+
+const properties = [
+	{
+		tag: "Rent",
+		title: "4 Beddroom Detached Duplex",
+		location : "Ire Akari Estate, Akala Express, Oluyole Extension, Ibadan, Oyo",
+		description : "",
+		price:  150000,
+		image :{
+			main: "",
+			sub: "",
+			others: ""
+		},
+
+	},
+	{
+		tag: "Sale",
+		title: "4 Beddroom Detached Duplex",
+		location:  "Ire Akari Estate, Akala Express, Oluyole Extension, Ibadan, Oyo",
+		description: "",
+		price: 150000,
+		image :{
+			main: "",
+			sub: "",
+			others: ""
+		},
+
+	}, {
+		tag: "Lease",
+		title: "4 Beddroom Detached Duplex",
+		location:  "Ire Akari Estate, Akala Express, Oluyole Extension, Ibadan, Oyo",
+		description: "",
+		price: 150000,
+		image :{
+			main: "/assets/",
+			sub: "",
+			others: ""
+		},
+
+	}
+];
+
+console.log((properties[0].title));
